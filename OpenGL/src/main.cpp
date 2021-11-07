a3f06724f06e9702bbf6c5de375758da7e5fade4
Ricardo Doberstein <ricardodoberstein@gmail.com>
2021-11-07 18:53:57 -0300

    Matrixes multiplication to simulate camera movement

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -27,7 +27,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -49,10 +49,10 @@ int main(void)
     {
 
         float positions[] = {
-            -0.5f, -0.5f, 0.0f, 0.0f, //0
-             0.5f, -0.5f, 1.0f, 0.0f, //1
-             0.5f,  0.5f, 1.0f, 1.0f, //2
-            -0.5f,  0.5f, 0.0f, 1.0f, //3
+            100.0f, 100.0f, 0.0f, 0.0f, //0
+            200.0f, 100.0f, 1.0f, 0.0f, //1
+            200.0f, 200.0f, 1.0f, 1.0f, //2
+            100.0f, 200.0f, 0.0f, 1.0f, //3
         };
 
         unsigned int indices[] = {
@@ -72,12 +72,15 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);  
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/button.png");
         texture.Bind();
