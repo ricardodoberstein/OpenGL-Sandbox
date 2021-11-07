e8d9f59645b21c94feec974838a40024fcbc24e3
Ricardo Doberstein <ricardodoberstein@gmail.com>
2021-11-07 17:45:08 -0300

    Texture loading and rendering

---
+++ OpenGL/src/Texture.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Renderer.h"
+
+class Texture
+{
+public:
+	Texture(const std::string& filepath);
+	~Texture();
+
+	void Bind(unsigned int slot = 0) const;
+	void Unbind() const;
+
+	inline int GetWidth() const { return m_Width; }
+	inline int GetHeight() const { return m_Height; }
+
+private:
+	unsigned int m_RendererID;
+	std::string m_FilePath;
+	unsigned char* m_LocalBuffer;
+	int m_Width;
+	int m_Height;
+	int m_BPP;
+};
